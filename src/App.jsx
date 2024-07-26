170dd04fb8056704992e355bce815d7fd9b011ee
R3ISilva <joaosilvaprivado@gmail.com>
2024-07-26 20:49:27 +0100

    removed old page, added sample page from vite mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/charts/styles.css';
 import { MantineProvider } from '@mantine/core';
+import {Welcome} from "./components/Welcome/Welcome.jsx";
+import {ColorSchemeToggle} from "./components/ColorSchemeToggle/ColorSchemeToggle.jsx";
 
 
 function App() {
@@ -16,28 +15,10 @@ function App() {
 function RealApp() {
     const [count, setCount] = useState(0)
     return (
-      <>
-          <div>
-              <a href="https://vitejs.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-          </div>k
-          <h1>Vite + React</h1>
-          <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-              <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
-          </div>
-          <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-          </p>
-      </>
+        <>
+            <Welcome />
+            <ColorSchemeToggle />
+        </>
     )
 }
 
